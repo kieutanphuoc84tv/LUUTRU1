d081eec275433d3b085a767b345244b6f6d96ce4
pdk <phandangkhoawork@gmail.com>
2022-10-27 15:58:15 +0700

    Xaydunghamin

---
+++ BTH1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Xay dung ham in ra man hinh chuoi
+
+void hello(){
+    printf("Hello World");
+}
+int main()
+{  
+   hello();
+   return 0;
+}
+
+// Xay dung ham tinh giai thua cua mot so nguyen duong  
+ 
+
