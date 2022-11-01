a62461b0f7df1ea35bf5b0b94adf932c93df7cd9
pdk <phandangkhoawork@gmail.com>
2022-11-01 21:14:25 +0700

    giaithua

--- BTH1.cpp
+++ BTH1.cpp
@@ -1,16 +1,37 @@
 #include <stdio.h>
+long tinhgiaithua (int n);
+int n;
 
-// Xay dung ham in ra man hinh chuoi
+// Xay dung ham in ra man hinh chuoi Hello World
 
-void hello(){
+void hello()
+{
     printf("Hello World");
 }
 int main()
 {  
-   hello();
-   return 0;
+   hello(); 
+   
+   tinhgiaithua(n);
+   
+return 0;
+   
 }
 
-// Xay dung ham tinh giai thua cua mot so nguyen duong  
+// Xay dung ham tinh giai thua cua mot so nguyen duong (vong lap)
  
+long tinhgiaithua (int n)
+{
+	int giaithua=1;
+	
+	do{
+		printf("\n\n\n Nhap so nguyen duong n de tinh giai thua:");
+		scanf("%d",&n);
+	}while(n<0 || n>666 );
+	
+	for(int i=1; i<=n;i++)
+		giaithua=giaithua*i;
+		printf("\n Giai thua cua %d = %d",n,giaithua);
+return 0;	
+}
 
